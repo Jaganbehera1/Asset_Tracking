bb024b586bc289824a26f074eb50eb286d4a876a
Jaganbehera1 <113991572+Jaganbehera1@users.noreply.github.com>
2025-04-10 23:23:55 +0530

    main.py

--- server/main.py
+++ server/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://asset-tracking-eight.vercel.app", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
